7f321dabcb51be6d91987910b41b71294713aa54
unknown <hamidazad020@gmail.com>
2024-02-13 20:41:00 +0600

    updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const fetcher = axios.create({
-  baseURL: "http://localhost:3001",
-  // baseURL: "https://resi-route-server.vercel.app",
+  // baseURL: "http://localhost:3001",
+  baseURL: "https://resi-route-server.vercel.app",
 });
 
 export default fetcher;
